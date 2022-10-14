864dd75ac595eca536874aeedb72eba2cc347d77
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-10-14 14:58:58 +0530

    init changed

--- init_setup.sh
+++ init_setup.sh
@@ -2,7 +2,7 @@ echo [$(date)]: "START"
 echo [$(date)]: "Creating env with python 3.8 version"
 conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "Activating the environment"
-conda activate ./env
+source activate ./env
 echo [$(date)]: "Installing the dev env"
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"
