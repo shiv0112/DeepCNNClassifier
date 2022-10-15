9b862db06891bab2a617cea39dfe437d634166ea
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-10-15 18:05:29 +0530

    stage1 init

--- src/DeepCNNClassifier/utils/common.py
+++ src/DeepCNNClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from deepClassifier import logger
+from DeepCNNClassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
