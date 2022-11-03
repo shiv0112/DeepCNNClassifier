f3012657646aecc4feff22b927a6c6197edc547e
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-11-03 22:27:33 +0530

    prediction final commit

--- prediction_service/app.py
+++ prediction_service/app.py
@@ -5,7 +5,7 @@ import numpy as np
 """
 # deep Classifier project
 """
-model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model("artifacts/training/model.h5")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
